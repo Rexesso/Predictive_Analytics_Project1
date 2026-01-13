6556bffc918c0dcd09c387600f5613cc72d2156a
Rexesso <rexesso@yahoo.com>
2026-01-12 21:31:27 -0600

    EDA and problems staging

--- Src/Exception.py
+++ Src/Exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail:sys):
@@ -25,4 +25,4 @@ if __name__ == "__main__":
         a=1/0
     except Exception as e: 
         logging.info("Divide by zero")
-        raise CustomException (e, sys)  """
+        raise CustomException (e, sys) """
